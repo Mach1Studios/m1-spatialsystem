8d61ae90eaa842b07725f5e7ab5743a69756230a
Dylan Marcus <dylan@dylanjmarcus.com>
2023-10-26 01:06:27 -0400

    fixed the postinstall script

--- installer/osx/om_bootstrap.sh
+++ installer/osx/om_bootstrap.sh
@@ -11,18 +11,21 @@ then
 	#echo "Bootstrapping the watcher utility as user $(id -u)"
 	#launchctl bootstrap gui/$(id -u) /Library/LaunchAgents/com.mach1.spatial.watcher.plist
 
-    # Run postinstall actions for root.
-    echo "Executing postinstall..."
-    # Add commands to execute in system context here.
+    if [ "$3" == "/" ]; then
+        # Run postinstall actions for root.
+        echo "Executing postinstall..."
+        # Add commands to execute in system context here.
 
-    # Run postinstall actions for all logged in users.
-    for pid_uid in $(ps -axo pid,uid,args | grep -i "com.mach1.spatial.watcher" | awk '{print $1 "," $2}'); do
-        pid=$(echo $pid_uid | cut -d, -f1)
-        uid=$(echo $pid_uid | cut -d, -f2)
-        # Replace echo with e.g. launchctl load.
-        echo "Executing postinstall for $uid"
-        echo "launchctl bsexec "$pid" chroot -u "$uid""
-        launchctl bsexec "$pid" chroot -u "$uid"
-    done
-
+        # Run postinstall actions for all logged in users.
+        for pid_uid in $(ps -axo pid,uid,args | grep -i "[l]oginwindow.app" | awk '{print $1 "," $2}'); do
+            pid=$(echo $pid_uid | cut -d, -f1)
+            uid=$(echo $pid_uid | cut -d, -f2)
+            # Replace echo with e.g. launchctl load.
+            echo "Executing postinstall for $uid"
+            echo "launchctl bootstrap -p gui/"$uid" /Library/LaunchAgents/com.mach1.spatial.watcher.plist"
+            launchctl bootstrap -p gui/"$uid" /Library/LaunchAgents/com.mach1.spatial.watcher.plist
+        done
+    fi
 fi
+
+exit 0
