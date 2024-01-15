962bb95e412257ae4fd81f10f66b039d409f9594
Dylan Marcus <dylan@mach1.tech>
2024-01-15 18:35:24 -0500

    updated service scripts to replace last installation and run once installed

--- installer/win/service_setup.bat
+++ installer/win/service_setup.bat
@@ -3,10 +3,13 @@ rem # Services Setup
 
 rem # Setup the m1-system-helper service
 if exist "%ProgramData%\Mach1\" (
+	sc query M1-System-Helper-Service >nul && sc delete M1-System-Helper-Service
 	sc create M1-System-Helper-Service binPath= "%ProgramData%\Mach1\m1-system-helper.exe" DisplayName= "M1-System-Helper" start= auto
+	sc start M1-System-Helper-Service
 )
 
 rem # Setup the m1-orientationmanager service
 if exist "%ProgramData%\Mach1\" (
+	sc query M1-OrientationManager >nul && sc delete M1-OrientationManager
 	sc create M1-OrientationManager binPath= "%ProgramData%\Mach1\m1-orientationmanager.exe" DisplayName= "M1-OrientationManager"
 )
--- installer/win/service_stopper.bat
+++ installer/win/service_stopper.bat
@@ -2,7 +2,7 @@ rem # MACH1 SPATIAL SYSTEM
 rem # Services Stopper
 
 rem # Stop the m1-system-helper service
-net stop "M1-System-Helper-Service"
+sc stop "M1-System-Helper-Service"
 
 rem # Stop the m1-orientationmanager service
-net stop "M1-OrientationManager"
+sc stop "M1-OrientationManager"
