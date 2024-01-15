6971f9f14dca2c44a600a7ac6e5bcc4bafb4100e
Dylan Marcus <dylan@mach1.tech>
2024-01-15 15:18:08 -0500

    fixed error on script blocking execution

--- installer/win/player_setup.bat
+++ installer/win/player_setup.bat
@@ -8,7 +8,6 @@ if exist "%ProgramFiles(x86)%\Mach1 Spatial System\M1-VideoPlayer.exe" (
 	MKDIR "%ProgramFiles%\Mach1"
 	MKDIR "%ProgramFiles%\Mach1\legacy"
 	MOVE /Y "%ProgramFiles(x86)%\Mach1 Spatial System\M1-VideoPlayer.exe" "%ProgramFiles%\Mach1\legacy\M1-VideoPlayer.exe"
-) else (
 )
 
 rem # download and install ffmpeg libs
@@ -23,14 +22,17 @@ rem ) else (
 rem     set ff_ver=%1
 rem )
 
-echo downloading ffmpeg 6 1>&2
+echo downloading ffmpeg 1>&2
 curl -L "https://github.com/GyanD/codexffmpeg/releases/download/6.1.1/ffmpeg-6.1.1-full_build-shared.zip" -o "%ProgramFiles%\Mach1\ffmpeg.zip"
 powershell Expand-Archive '%ProgramFiles%\Mach1\ffmpeg.zip' -DestinationPath '%ProgramFiles%\Mach1'
 del "%ProgramFiles%\Mach1\ffmpeg.zip"
 ren "%ProgramFiles%\Mach1\ffmpeg-6.1.1-full_build-shared" "ffmpeg"
 
 echo copying ffmpeg dlls 1>&2
-copy "%ProgramFiles%\Mach1\ffmpeg\bin\av*.dll" "%ProgramFiles%\Mach1"
-copy "%ProgramFiles%\Mach1\ffmpeg\bin\postproc*.dll" "%ProgramFiles%\Mach1"
-copy "%ProgramFiles%\Mach1\ffmpeg\bin\sw*.dll" "%ProgramFiles%\Mach1"
-rmdir /s /q %ProgramFiles%\Mach1\ffmpeg
+copy /Y "%ProgramFiles%\Mach1\ffmpeg\bin\avformat*.dll" "%ProgramFiles%\Mach1"
+copy /Y "%ProgramFiles%\Mach1\ffmpeg\bin\avutil*.dll" "%ProgramFiles%\Mach1"
+copy /Y "%ProgramFiles%\Mach1\ffmpeg\bin\avcodec*.dll" "%ProgramFiles%\Mach1"
+copy /Y "%ProgramFiles%\Mach1\ffmpeg\bin\swscale*.dll" "%ProgramFiles%\Mach1"
+
+echo removing downloaded ffmpeg dir 1>&2
+rmdir /s /q "%ProgramFiles%\Mach1\ffmpeg"
