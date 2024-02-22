86f55711031b86f68fd5e470057db8360bc3429e
Dylan Marcus <dylan@dylanjmarcus.com>
2024-02-22 17:44:58 -0500

    minor script fixes for v1 uninstaller

--- installer/osx/v1_uninstaller.sh
+++ installer/osx/v1_uninstaller.sh
@@ -4,29 +4,29 @@
 # Uninstall 1.x.x Mach1 Spatial System
 
 # Remove installed files from .pkg
-rm -rf /Applications/Mach1\ Spatial\ System
+sudo rm -rf /Applications/Mach1\ Spatial\ System
 rm -rf ~/Library/Application\ Support/Mach1\ Spatial\ System
 
-rm -rf /Library/Application Support/Avid/Audio/Plug-Ins/M1-Monitor.aaxplugin
-rm -rf /Library/Application Support/Avid/Audio/Plug-Ins/M1-Panner.aaxplugin
+sudo rm -rf /Library/Application Support/Avid/Audio/Plug-Ins/M1-Monitor.aaxplugin
+sudo rm -rf /Library/Application Support/Avid/Audio/Plug-Ins/M1-Panner.aaxplugin
 
 rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Monitor.component
-rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner(1->8).component
-rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner(2->8).component
-rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner(4->8).component
-rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner(6->8).component
+rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner\(1-\>8\).component
+rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner\(2-\>8\).component
+rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner\(4-\>8\).component
+rm -rf ~/Library/Audio/Plug-Ins/Components/M1-Panner\(6-\>8\).component
 
 rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Monitor.vst
-rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner(1->8).vst
-rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner(2->8).vst
-rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner(4->8).vst
-rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner(6->8).vst
+rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner\(1-\>8\).vst
+rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner\(2-\>8\).vst
+rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner\(4-\>8\).vst
+rm -rf ~/Library/Audio/Plug-Ins/VST/M1-Panner\(6-\>8\).vst
 
 rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Monitor.vst3
-rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner(1->8).vst3
-rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner(2->8).vst3
-rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner(4->8).vst3
-rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner(6->8).vst3
+rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner\(1-\>8\).vst3
+rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner\(2-\>8\).vst3
+rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner\(4-\>8\).vst3
+rm -rf ~/Library/Audio/Plug-Ins/VST3/M1-Panner\(6-\>8\).vst3
 
 # Remove files made by the operating system during runtime
 rm -rf ~/Library/Application\ Support/M1-Transcoder
