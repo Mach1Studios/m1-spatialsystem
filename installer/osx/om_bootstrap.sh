bdd76a7f2646488f3e9bfaeb8964a871139e7304
Dylan Marcus <dylan@dylanjmarcus.com>
2025-11-05 15:05:08 -0500

    update pt template

--- installer/osx/om_bootstrap.sh
+++ installer/osx/om_bootstrap.sh
@@ -5,6 +5,9 @@
 
 # TODO: write a test for macos 10.7->10.9 to use load command instead of bootstrap
 
+# Ensure log directory exists
+mkdir -p "/Library/Application Support/Mach1"
+
 if [ -f "/Library/LaunchAgents/com.mach1.spatial.helper.plist" ]
 then 
     # Run postinstall actions for all logged in users.
@@ -15,6 +18,7 @@ then
     sudo chmod 755 /Library/Application\ Support/Mach1/m1-system-helper
     sudo chmod 755 /Library/Application\ Support/Mach1/m1-orientationmanager
 
+    # Get logged in user UIDs
     for uid in $(ps -axo uid,args | grep -i "[l]oginwindow.app" | awk '{ print $1 }')
     do
         echo "Executing postinstall for $uid" > /Library/Application\ Support/Mach1/m1-spatialsystem.log
@@ -22,6 +26,8 @@ then
         #launchctl kickstart -kp gui/"$uid"/com.mach1.spatial.helper
         launchctl bootstrap gui/"$uid" /Library/LaunchAgents/com.mach1.spatial.orientationmanager.plist
     done
+    
+    echo "Postinstall completed for services" >> /Library/Application\ Support/Mach1/m1-spatialsystem.log
 fi
 
 exit 0
